af989f8132a787669c28cd31025dedd2dc072814
Ahmed abdulrazzaq <aabdulra19@bw.edu>
2022-09-18 13:53:37 -0400

    added errorHandler middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
-
+const { errorHandler } = require("./middleware/errorHandler");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 8000;
 app.get("/", (req, res) => {
   res.send({
@@ -12,6 +14,7 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/users", require("./routes/userRoute"));
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`.brightMagenta);
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,11 @@
 //@Route /API/USERS/login
 //@Access public
 const loginUser = (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("please include all fields");
+  }
   res.status(200).json({ message: "user controller" });
 };
 
