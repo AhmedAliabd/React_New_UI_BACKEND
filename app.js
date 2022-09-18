8130960e2584f990cb354cb7b293fd9134da38a0
Ahmed abdulrazzaq <aabdulra19@bw.edu>
2022-09-18 12:05:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome!",
+  });
+});
+
+app.listen(8000, () => {
+  console.log("SERVER RUNNING");
+});
