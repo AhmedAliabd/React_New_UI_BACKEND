e5fdea45cc355895afddbd801f67603992521600
Ahmed abdulrazzaq <aabdulra19@bw.edu>
2022-09-20 00:15:08 -0400

    clean up

--- app.js
+++ app.js
@@ -4,22 +4,24 @@ const dotenv = require("dotenv").config();
 const DB = require("./configs/db");
 const { errorHandler } = require("./middleware/errorHandler");
 
-//Connect DB
+//Connect DB 💰
 DB();
+
 const app = express();
+//use middleware section 🐛
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const PORT = process.env.PORT || 8000;
-app.get("/", (req, res) => {
-  res.send({
-    message: "Welcome!",
-  });
-});
 
-//Routes
+const PORT = process.env.PORT || 8000;
+
+//Routes 🚆
 app.use("/api/users", require("./routes/userRoute"));
 app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`.brightMagenta);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(`uncaughtException => ${err.message}`.red.bold);
 });
