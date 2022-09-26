444d502ea4df3fb58eaa5c2a4f979418959cd17b
Ahmed abdulrazzaq <aabdulra19@bw.edu>
2022-09-26 01:01:03 -0400

    enabled cross

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 8000;
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.get("/", (req, res) => {
   res.json({ message: "working" });
 });
