3abbec298c45197599ee051200a97c5bd256f633
Ahmed abdulrazzaq <aabdulra19@bw.edu>
2022-09-26 01:29:53 -0400

    update

--- app.js
+++ app.js
@@ -13,9 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 8000;
-app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://vast-chamber-75472.herokuapp.com/*"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 app.get("/", (req, res) => {
